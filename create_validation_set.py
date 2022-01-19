f92023b6ece35a14ec5e693dbe9358828d8e5c47
Spencer Ng <sng1488@gmail.com>
2022-01-18 23:53:50 -0600

    Update validation set and noise injection specs

--- create_validation_set.py
+++ create_validation_set.py
@@ -4,10 +4,10 @@ Write a script that takes in labeled data in data/train/ and
 outputs the validation dataset in data/validation/.
 
 You may also want to create a modified data/train/ folder
-(or a modified copy of the folder) that does not include you validation
+(or a modified copy of the folder) that does not include your validation
 data. Be sure to document how to run this script here or in your report.
 
 Your validation set should contain at least 10 samples of IMU data per
-activity, with each sample being 5+ seconds long.
+activity, with each sample being 7+ seconds long.
 
 """
--- inject_noise.py
+++ inject_noise.py
@@ -1,8 +1,10 @@
 """
 
 Write a script that takes your validation set, injects Gaussian noise of different
-variances, and saves this set to a separate directory. 
+variances, and saves this set to a separate directory. If your group is also completing
+the bonus task, this script should also be able to inject noise on the test set.
 
-Be sure to include documentation with clear instructions on how to run the script.
+Be sure to include documentation with clear instructions on how to run the script
+here or in your report.
 
 """
