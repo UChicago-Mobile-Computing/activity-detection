5f64eaac67b98e7032d80d703ad9d126c2ea1000
Spencer Ng <spencerng@uchicago.edu>
2022-01-19 23:26:32 -0500

    Edit validation set requirements

--- create_validation_set.py
+++ create_validation_set.py
@@ -7,7 +7,7 @@ You may also want to create a modified data/train/ folder
 (or a modified copy of the folder) that does not include your validation
 data. Be sure to document how to run this script here or in your report.
 
-Your validation set should contain at least 10 samples of IMU data per
-activity, with each sample being 7+ seconds long.
+Your validation set should contain at least 8 samples of IMU data per
+activity, with each sample being 12+ seconds long.
 
 """
