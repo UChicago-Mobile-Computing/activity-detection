71bd100a1ffcb78aada80aa6d98fd91eaaddec6a
Zain Sarwar <66789976+zainsarwar865@users.noreply.github.com>
2022-02-01 20:06:34 -0600

    Update run_limu_bert_val.sh

--- run_limu_bert_val.sh
+++ run_limu_bert_val.sh
@@ -2,4 +2,4 @@ python3 limu_bert/dataset/motion_final_val.py --data_folder=$1
 python3 limu_bert/embedding.py v1 motion 20_120 -f motion \
 	--train_cfg limu_bert/config/pretrain.json --mask_cfg limu_bert/config/mask.json
 python3 limu_bert/classifier_final_val.py v2 motion 20_120 -f motion -s motion -l 0 \
-	--train_cfg limu_bert/config/train.json --output_predictions=$2
+	--train_cfg limu_bert/config/train.json
